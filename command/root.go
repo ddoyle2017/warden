6300c4239a75826d8621d30c79cc539fced2daae
Daniel Doyle <ddoyle4@uic.edu>
2024-04-05 15:27:33 -0500

    root command clean-up

--- command/root.go
+++ command/root.go
@@ -2,15 +2,14 @@ package command
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/spf13/cobra"
 )
 
 var rootCommand = &cobra.Command{
-	Use: "warden",
+	Use:   "warden",
 	Short: "Warden is a CLI mod manager for Valheim",
-	Long: `A fast and friendly CLI mod manager for Valheim. Built with love and Go for handling mods on headless servers <3`,
+	Long:  `A fast and friendly CLI mod manager for Valheim. Built with love and Go for handling mods on headless servers <3`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println(logo)
 		fmt.Println(startUpBlurb)
@@ -18,8 +17,5 @@ var rootCommand = &cobra.Command{
 }
 
 func Execute() {
-	if err := rootCommand.Execute(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
+	cobra.CheckErr(rootCommand.Execute())
 }
