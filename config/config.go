6f82af4c064998a157ac00c0f93f8522979ddd82
Daniel Doyle <ddoyle4@uic.edu>
2024-04-23 14:02:27 -0500

    reworked config package

--- config/config.go
+++ config/config.go
@@ -9,7 +9,15 @@ import (
 	"github.com/spf13/viper"
 )
 
-const wardenConfigFile = ".warden.yaml"
+const (
+	wardenConfigFile = ".warden.yaml"
+
+	defaultSteamInstallPath = ".steam/SteamApps/common/Valheim dedicated server"
+
+	// BepInEx is required by practically every mod for Valheim, so we use it
+	// for the default path
+	defaultModInstallPath = "/BepInEx/plugins"
+)
 
 var (
 	ErrUnableToWriteConfig = errors.New("unable to write to config file")
@@ -19,58 +27,52 @@ var (
 // Config represents all configurable values needed to make warden work, e.g. the directory
 // to install mods to
 type Config struct {
-	ModDirectory string `mapstructure:"mod-directory"`
+	ValheimDirectory string `mapstructure:"valheim-directory"`
+	ModDirectory     string `mapstructure:"mod-directory"`
 }
 
-type Option func(*Config)
-
-func New(options ...Option) *Config {
-	home, err := homedir.Dir()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	c := &Config{
-		ModDirectory: home,
-	}
-	for _, opt := range options {
-		opt(c)
-	}
-	return c
-}
-
-// The folder where all mods are installed
-func WithModDirectory(directory string) Option {
-	return func(c *Config) {
-		c.ModDirectory = directory
-	}
-}
-
-// LoadConfig looks for and loads in the .warden.yaml configuration file at
-// the specified path
-func (c *Config) LoadConfig(path string) error {
+// Load creates a new instance of Config, based on a configuration YAML file at the given
+// path. If one doesn't exist, a new file is created with default values
+func Load(path string) (*Config, error) {
 	viper.AddConfigPath(path)
 	viper.SetConfigName(".warden")
 	viper.SetConfigType("yaml")
 
 	err := viper.ReadInConfig()
-	if errors.As(err, &viper.ConfigFileNotFoundError{}) {
-		createDefaultConfig(path)
-	} else if err != nil {
-		return ErrFailedToReadConfig
+	cfg := &Config{
+		ValheimDirectory: getDefaultValheimDirectory(),
+		ModDirectory:     defaultModInstallPath,
 	}
 
-	cfg := Config{}
-	if err := viper.Unmarshal(&cfg); err != nil {
-		return ErrFailedToReadConfig
+	// If config doesn't exist, create the file and add default values
+	if errors.As(err, &viper.ConfigFileNotFoundError{}) {
+		createConfigFile(cfg, path)
+	} else if err != nil {
+		return nil, ErrFailedToReadConfig
 	}
-	return nil
+
+	// Load in settings from YAML file into Config struct
+	if err := viper.Unmarshal(&cfg); err != nil {
+		return nil, ErrFailedToReadConfig
+	}
+	return cfg, nil
 }
 
-// createDefaultConfig creates a new configuration file called .warden.yaml
-func createDefaultConfig(path string) error {
-	file := filepath.Join(path, wardenConfigFile)
+// Returns the default installation path used by SteamCMD for Linux
+func getDefaultValheimDirectory() string {
+	home, err := homedir.Dir()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return filepath.Join(home, defaultSteamInstallPath)
+}
 
+// Creates a new configuration file called .warden.yaml, with the given settings
+func createConfigFile(cfg *Config, path string) error {
+	viper.Set("valheim-directory", cfg.ValheimDirectory)
+	viper.Set("mod-directory", cfg.ModDirectory)
+
+	file := filepath.Join(path, wardenConfigFile)
 	if err := viper.WriteConfigAs(file); err != nil {
 		return ErrUnableToWriteConfig
 	}
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 	"warden/api/thunderstore"
 	"warden/command"
 	"warden/config"
@@ -11,14 +12,19 @@ import (
 	"warden/data/file"
 	"warden/data/repo"
 	"warden/service"
+
+	"github.com/mitchellh/go-homedir"
 )
 
 func main() {
 	// Load in the config
-	cfg := config.New(
-		config.WithModDirectory("./test/files"),
-	)
-	if err := cfg.LoadConfig("."); err != nil {
+	home, err := homedir.Dir()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cfg, err := config.Load(home)
+	if err != nil {
 		log.Fatal(err.Error())
 	}
 
@@ -30,9 +36,11 @@ func main() {
 	data.CreateModsTable(db)
 
 	// Initialize and injection dependencies into commands
+	md := filepath.Join(cfg.ValheimDirectory, cfg.ModDirectory)
+
 	r := repo.NewModsRepo(db)
 	ts := thunderstore.New(&http.Client{})
-	fm := file.NewManager(cfg.ModDirectory, &http.Client{})
+	fm := file.NewManager(md, &http.Client{})
 
 	modService := service.NewModService(r, fm, ts, os.Stdin)
 
